db85833e8b6dd07eb93d15865b7074646392a10f
beenleyi <iyibeenle@gmail.com>
2020-07-01 14:44:56 +0800

    console program finish

--- init.cpp
+++ init.cpp
@@ -1,47 +1,41 @@
 #include"struct.h"
-#include<vector>
-#include<iostream>
-#include <string>
-#include<fstream>
-using namespace std; 
-extern city cities[20];
 
-void read_vehicle(ifstream & in_data,int n){
+extern city cities[CITY_NUM];
+
+int get_city_no_vehicle_data(string c){
+	int i;
+	for (i = 0; i < total_city; i++){
+		if (c == cities[i].city_name) break;
+	}
+	if (i != total_city) return i; 
+	else {
+		cout<<"****ERROR CITY NAME IN THE VEHICLE DATA****\n";
+		return -1;
+	}
+}
+
+void read_vehicle(ifstream & in_data, int type){
 	string c;  
 	int i;
 	vehicle temp;
 	while (in_data>>c){
-		for (i = 0; i < n; i++){
-			if (cities[i].city_name == c) break;
-		}
-		if (i == n) {
-			printf("There is a wrong city_name in the vehicle data\n");
-			continue;
-		}
-		temp.type = 1;
+		i = get_city_no_vehicle_data(c);
+		if (i==-1) continue;
+		temp.type = type;
 		in_data.ignore(1); 
-		in_data >> temp.dst_city;
+		in_data >> c;
+		temp.dst_city_no = get_city_no_vehicle_data(c);
+		if (temp.dst_city_no==-1) continue;
 		in_data.ignore(1); 
-		in_data >> temp.start_time.year;
-		in_data.ignore(1); 
-		in_data >> temp.start_time.month;
-		in_data.ignore(1); 
-		in_data >> temp.start_time.day;
 		in_data >> temp.start_time.hour;
-		in_data.ignore(6);
-		in_data >> temp.end_time.year;
+		in_data.ignore(1);
+		in_data >> temp.start_time.minute;
 		in_data.ignore(1); 
-		in_data >> temp.end_time.month;
-		in_data.ignore(1); 
-		in_data >> temp.end_time.day;
-		in_data >> temp.end_time.hour;
-		in_data.ignore(6);		
+		in_data >> temp.time_spent.hour;
+		in_data.ignore(1);
+		in_data>>temp.time_spent.minute;
 		cities[i].my_vehicle.push_back(temp);
-		cout<<cities[i].city_name<<temp.dst_city;
-		printf("%d-%d-%d ", temp.start_time.year, temp.start_time.month, temp.start_time.day);
-		printf("%d:00:00\n", temp.start_time.hour);
-		printf("%d-%d-%d ", temp.end_time.year, temp.end_time.month, temp.end_time.day);
-		printf("%d:00:00\n",temp.end_time.hour);
+		//cout<<cities[i].city_name<<" "<<cities[temp.dst_city_no].city_name<<" "<<temp.start_time.hour<<":"<<temp.start_time.minute;
 	}
 }
 
@@ -51,21 +45,21 @@ int init(){
 	int n = 0, risk_num, i;
 	city_data.open("city.data");
 	while(city_data>>cities[n].city_name){
-		city_data>>cities[n].city_risk;
+		city_data>>cities[n].city_risk_no;
 		n++;
 	}
 	city_data.close();
 	ifstream plane_data;
 	plane_data.open("plane.data");
-	read_vehicle(plane_data, n);
+	read_vehicle(plane_data, 0);
 	plane_data.close();
 	ifstream train_data;
 	train_data.open("train.data");
-	read_vehicle(train_data, n);
+	read_vehicle(train_data, 1);
 	train_data.close();
 	ifstream car_data;
 	car_data.open("car.data");
-	read_vehicle(car_data, n);	
+	read_vehicle(car_data, 2);	
 	car_data.close();
 	return n;
 }
